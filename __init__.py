86d2cc874c7ee44afc156b626e6a195b10992811
0edk <232494884+0edk@users.noreply.github.com>
2026-06-30 10:27:39 -0400

    refactor: split GraphTopology into its own file

--- __init__.py
+++ __init__.py
@@ -1,62 +1,10 @@
-import itertools
-from typing import Iterable, Optional
+from typing import Optional
 
 import aqt
-from aqt.utils import tooltip, show_warning
-from anki.models import TemplateDict
-from anki.notes import Note, NoteId
+from aqt.utils import show_warning
+from anki.notes import Note
 
-from .flashcard_topology import indices, NoteTopology, TopologyDialog
-from .gui import GraphViewDialog
-from .models import make_edge, name_edge
-
-class GraphTopology(NoteTopology):
-    @staticmethod
-    def description() -> str:
-        return "Directed Graph"
-
-    def make_templates(self, order: int) -> Iterable[TemplateDict]:
-        manager = self.mw.col.models
-        return itertools.chain(*((lambda i=i: (
-            make_edge(manager, i, j) for j in indices(order) if i != j
-        ))() for i in indices(order)))
-
-    @staticmethod
-    def make_fields(order: int) -> Iterable[str]:
-        fields = ["Context", "Source"]
-        for i in indices(order):
-            fields.append(f"Node {i}")
-            fields.extend(itertools.chain(*(
-                (name_edge(j, i), name_edge(i, j))
-                for j in range(1, i) if i != j
-            )))
-        return fields
-
-    def custom_css(self, order: int) -> str:
-        return ""
-
-    @staticmethod
-    def next_order(order: Optional[int] = None) -> int:
-        return 2 if order is None else order + 1
-
-    @staticmethod
-    def measure_order(fields: dict[str, str]) -> int:
-        i = 1
-        while f"Node {i}" in fields and (i == 1 or f"Edge 1 {i}" in fields):
-            i += 1
-        return i - 1
-
-    @classmethod
-    def blank_example(cls) -> dict[str, str]:
-        example = super().blank_example()
-        example["Node 1"] = "A"
-        example["Node 2"] = "B"
-        return example
-
-    def make_editor(
-        self, fields: dict[str, str], note_id: Optional[NoteId]
-    ) -> TopologyDialog:
-        return GraphViewDialog(fields, note_id, self)
+from .topo import GraphTopology
 
 GraphTopology(aqt.mw)
 
